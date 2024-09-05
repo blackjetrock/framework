0f4a95a136b47522838cb5cd077888ec28c3144d
menadue <menadue@gmail.com>
2024-09-05 12:57:57 +0100

    update

---
+++ framework_16/macropad/v1/macropad1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// GPIOs
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+const uint PIN_SDAOUT     = 14;
+const uint PIN_LATCHOUT2  = 15;
+const uint PIN_I2C_SDA    = 16;
+const uint PIN_I2C_SCL    = 17;
+const uint PIN_LS_DIR     = 18;
+const uint PIN_LATCHIN    = 19;
+const uint PIN_SCLKIN     = 20;
+const uint PIN_SDAIN      = 21;
+const uint PIN_LATCHOUT1  = 22;
+const uint PIN_SCLKOUT    = 26;
+const uint PIN_VBAT_SW_ON = 27;
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+void oledmain(void);
+
+int main() {
+
+    // Main loop
+    while(1)
+      {
+	
+      }
+    
+}
+
